a45ec68bf6b0c2a5618222b3c33f91fa004b6151
Liniker Silva <contato@liniker.com.br>
2016-04-11 10:42:37 -0300

    Update exercicio_01.php

--- exercicios/exercicio_01.php
+++ exercicios/exercicio_01.php
@@ -1,4 +1,5 @@
-1_ Construir um algoritmo que leia 2 números e efetue a adição. Caso o valor somado seja maior que 20, este deverá ser apresentando somando-se a ele mais 8.
-Caso o valor somado seja menor ou igual a 20, este deverá ser apresentado subtraindo-se 5.
+1_ Construir um algoritmo que leia 2 números e efetue a adição. Caso o valor somado seja maior que 20,
+este deverá ser apresentando somando-se a ele mais 8.Caso o valor somado seja menor ou igual a 20,
+este deverá ser apresentado subtraindo-se 5.
 
 Obs: Não será adminitido valor negativo
